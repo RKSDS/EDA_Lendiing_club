44e4ef911ac7dae845ce56371a230b345f8a1339
Ramesh Singh <ramesh.singh1986@gmail.com>
2017-06-24 00:46:02 +0530

    Cleaned and updated code

--- code/Garmener_Case_Study0.2.R
+++ code/Garmener_Case_Study0.2.R
@@ -114,8 +114,8 @@ sum(is.na(loan_ds$sub_grade))
 sum(loan_ds$sub_grade %in% c("", " "))
 #unique grades
 unique(loan_ds$sub_grade)
-#convert it to factor as this is an ordered category
-loan_ds$sub_grade <- as.factor(loan_ds$sub_grade)
+#Getting rid of this as grades covers the top level categorization
+remove_fields <- append(remove_fields, "sub_grade")
 
 
 
@@ -132,6 +132,7 @@ loan_ds$emp_title <- gsub("[[:punct:]]","",loan_ds$emp_title)
 length(unique(loan_ds$emp_title)) #26446
 #wont be of much use as employer directly do not make a person defaulter
 remove_fields <- append(remove_fields, "emp_title")
+
 
 
 #find Unique entries for employement duration
@@ -167,10 +168,13 @@ unique(loan_ds$home_ownership)
 loan_ds$home_ownership <- as.factor(loan_ds$home_ownership)
 
 
+
 #sanity check for annual income
 sum(is.na(loan_ds$annual_inc))
-sum(loan_ds$annual_inc==0)           
+sum(loan_ds$annual_inc==0)
+#getting rid of outliers
 loan_ds$annual_inc[loan_ds$annual_inc %in% boxplot.stats(loan_ds$annual_inc)$out] <- NA
+
 
 
 #Sanity check for verification status
@@ -178,9 +182,13 @@ sum(is.na(loan_ds$verification_status))
 sum(loan_ds$verification_status %in% c("", " "))
 #unique entries for income source verification status
 unique(loan_ds$verification_status)
+#converting to factors
 loan_ds$verification_status <- as.factor(loan_ds$verification_status)
 
-loan_ds %>% group_by(verification_status) %>% summarise(mean=mean(funded_amnt), median=median(funded_amnt), st_dev=sd(funded_amnt))
+#checking various the metrics across verification status
+loan_ds %>% group_by(verification_status) %>% summarise(mean=mean(funded_amnt), 
+                                                        median=median(funded_amnt), 
+                                                        st_dev=sd(funded_amnt))
 
 
 
@@ -195,13 +203,12 @@ issue_month <- format(loan_ds$issue_d, "%b")
 issue_year <- as.numeric(format(loan_ds$issue_d,"%Y"))
 #qet quarter from date
 issue_quarter <- quarters(loan_ds$issue_d)
+#combining all the derived data to main df
 loan_ds <- cbind(loan_ds, issue_month, issue_quarter, issue_year)
-
-unique(loan_ds$issue_month)
-loan_ds$issue_month <- factor(loan_ds$issue_month, levels=c("Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct","Nov",  "Dec"))
-#convert to date and analyse when the loans are getting release most
-
-
+#converting the month column to an ordered category
+loan_ds$issue_month <- factor(loan_ds$issue_month, 
+                              levels=c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                                       "Jul", "Aug", "Sep", "Oct","Nov",  "Dec"))
 
 
 
@@ -214,14 +221,14 @@ loan_ds$loan_status <- as.factor(loan_ds$loan_status)
 
 
 
-
-# dont have any use as this is as unique as id and doesn't make sense in analysis ----- this is for url
+# dont have any use as this is as unique as id and doesn't make sense in analysis
 remove_fields <- append(remove_fields, "url")
 
 
 
 # description doesn't make much sense here again.. this is personalised message from applicant.
-# it can be used by minning this to understand the underling sentiment/cause of request. But we are not executing it here  ------- Lets discuss this
+# it can be used by minning this to understand the underling sentiment/cause of request. But we are not 
+#executing it here
 remove_fields <- append(remove_fields, "desc")    
 
 
@@ -230,12 +237,15 @@ remove_fields <- append(remove_fields, "desc")
 sum(is.na(loan_ds$purpose))
 sum(loan_ds$purpose %in% c("", " "))
 unique(loan_ds$purpose)
+#converting to factor
 loan_ds$purpose <- as.factor(loan_ds$purpose)
+
+
 
 #Sanity check for loan title
 sum(is.na(loan_ds$title))
 sum(loan_ds$title %in% c("", " "))
-length(unique(loan_ds$title)) # may not be of much use.. Individual way of naming the loan
+length(unique(loan_ds$title)) # may not be of much use.. Individual way of naming the loan.. hence removing
 remove_fields <- append(remove_fields, "title")
 
 
@@ -248,7 +258,6 @@ remove_fields <- append(remove_fields, "zip_code")
 
 
 
-
 #Sanity check for addr_state
 sum(is.na(loan_ds$addr_state))
 sum(loan_ds$addr_state %in% c("", " "))
@@ -257,11 +266,13 @@ length(unique(loan_ds$addr_state))
 loan_ds$addr_state <- as.factor(loan_ds$addr_state)
 
 
+
 #Sanity check for dti
 #less value mean good possibility the payment will be done by borrower in time.
-# less mean less chances of defaulting
 sum(is.na(loan_ds$dti))
 sum(loan_ds$dti==0)
+
+
 
 #no. of unique delequency events  for past 2 years of loan account
 #the high the bad
@@ -278,20 +289,13 @@ sum(is.na(loan_ds$earliest_cr_line))
 sum(loan_ds$earliest_cr_line %in% c("", " "))
 unique(loan_ds$earliest_cr_line)
 #separate the year and month
-loan_ds$earliest_cr_line <- as.Date(paste("01-", loan_ds$earliest_cr_line, sep=""), format="%d-%b-%y")
-#get Month from date
-earliest_cr_line_month <- format(loan_ds$earliest_cr_line, "%b")
-#get year from date
-earliest_cr_line_year <- as.numeric(format(loan_ds$earliest_cr_line,"%Y"))
-#qet quarter from date
-earliest_cr_line_quarter <- quarters(loan_ds$earliest_cr_line)
-loan_ds <- cbind(loan_ds, earliest_cr_line_month, earliest_cr_line_quarter, earliest_cr_line_year)
+loan_ds <- separate(loan_ds, earliest_cr_line, into = c("crdit_ln_month", "credit_ln_year"), sep = "-")
+remove_fields <- append(remove_fields, "crdit_ln_month")
+loan_ds$credit_ln_year <- sapply(loan_ds$credit_ln_year, function(x) {if(as.numeric(x)>17) {paste("19",x, sep="")} else{paste("20",x,sep="")}})
+loan_ds$credit_ln_year <- as.numeric(loan_ds$credit_ln_year)
+sum(is.na(loan_ds$credit_ln_year))
 #convert to date and analyse when the loans are getting release most
 #if time permits will include in study
-
-loan_ds$earliest_cr_line_month <- as.factor(loan_ds$earliest_cr_line_month)
-loan_ds$earliest_cr_line_quarter <- as.factor(loan_ds$earliest_cr_line_quarter)
-loan_ds$earliest_cr_line_year <- as.factor(loan_ds$earliest_cr_line_year)
 
 
 
@@ -300,7 +304,6 @@ unique(loan_ds$inq_last_6mths)
 #All are valid values, no NA or blank value reported 
 #Converting to factor
 loan_ds$inq_last_6mths <- as.factor(loan_ds$inq_last_6mths)
-#More the no of inquiry more Credit Hungry which reduces the Credit rating of the Borrower.
 
 
 
@@ -341,14 +344,16 @@ loan_ds$pub_rec <- as.factor(loan_ds$pub_rec)
 
 
 
-#this is if low then the borrower is less risky
 #installment - payment for particular month
-#more mining bad
+#more mining is required here to give insight of any kind
 sum(is.na(loan_ds$revol_bal))
 sum(loan_ds$revol_bal==0)
+#removing as revol_util is better column to study utilization
+remove_fields <- append(remove_fields, "total_acc")
 
 
 
+#sanity check
 #here the if the credit used is less then the chances of making payment is more
 #but there is not too much profit to bank.
 #bank need to make sure the utilization happens and the payments is also done for it.
@@ -373,7 +378,8 @@ remove_fields <- append(remove_fields, "total_acc")
 #the less the better
 sum(is.na(loan_ds$out_prncp))
 sum(loan_ds$out_prncp==0)
-
+#getting rid of this as there is very limited insight for current scenario
+remove_fields <- append(remove_fields, "out_prncp")
 
 
 #The less the better
@@ -381,6 +387,7 @@ sum(loan_ds$out_prncp==0)
 #the difference in out_prncp_inv is what bank will keep.
 sum(loan_ds$out_prncp_inv==0)
 max(loan_ds$out_prncp_inv)
+#its more useful for individual analysis hence removing from here
 remove_fields <- append(remove_fields, "out_prncp_inv")
 
 
@@ -388,6 +395,8 @@ remove_fields <- append(remove_fields, "out_prncp_inv")
 #payments received to date for total funded amount
 sum(is.na(loan_ds$total_pymnt))
 sum(loan_ds$total_pymnt==0) #not good or recently loan started
+#this is also not useful column for current analysis
+remove_fields <- append(remove_fields, "total_pymnt")
 
 
 #total_funded - total_payment low = better
@@ -405,109 +414,100 @@ remove_fields <- append(remove_fields, "next_pymnt_d")
 #might not be useful as there are only two unique dates for the column and many are empty
 
 loan_ds$last_pymnt_d
-#mths_since_last_delinq
+#this is not useful column as it could be representing a closed cr line
+remove_fields <- append(remove_fields, "last_pymnt_d")
 
 
 
-# 10 -- Summary for pub_rec_bankruptcies (Number of public record bankruptcies)
-str(loan_ds$pub_rec_bankruptcies)
+#Summary for pub_rec_bankruptcies (Number of public record bankruptcies)
 sum(is.na(loan_ds$pub_rec_bankruptcies))  #No of NA in the column
 sum(loan_ds$pub_rec_bankruptcies %in% c("", " "))  #No of Blanks in the Column
-length(unique(loan_ds$pub_rec_bankruptcies))  # No of Unique values Present in Column
 unique(loan_ds$pub_rec_bankruptcies) # Listing Unique values
 loan_ds$pub_rec_bankruptcies <- as.factor(loan_ds$pub_rec_bankruptcies) # converting column as factor
 
-#9 -- Summary for last_credit_pull_d
-str(loan_ds$last_credit_pull_d)
+
+
+#Summary for last_credit_pull_d
 sum(is.na(loan_ds$last_credit_pull_d))
 sum(loan_ds$last_credit_pull_d %in% c("", " "))
 str(loan_ds$last_credit_pull_d)
+#this cannot be used as it might give very old date or date for closed loans
 
-loan_ds$last_credit_pull_d <- tolower(gsub(" ", "", loan_ds$last_credit_pull_d))
-loan_ds$last_credit_pull_d <- paste("01-",loan_ds$last_credit_pull_d, sep = "")
 
-loan_ds$last_credit_pull_d <- as.Date(loan_ds$last_credit_pull_d, format = "%d-%b-%y")
-str(loan_ds$last_credit_pull_d)
 
-#Dates doesn't need conversion to date.. we need month and year which can be easily retrieved without
-#converting to date.. -------- Discussion
-#8 -- Summary for next_pymnt_d (Next scheduled payment date)
-str(loan_ds$next_pymnt_d)
+#Summary for next_pymnt_d (Next scheduled payment date)
 sum(is.na(loan_ds$next_pymnt_d))
 sum(loan_ds$next_pymnt_d %in% c("", " ")) #38577
-loan_ds$next_pymnt_d <- tolower(gsub(" ", "", loan_ds$next_pymnt_d))
-loan_ds$next_pymnt_d <- paste("01-",loan_ds$next_pymnt_d, sep = "")
-
-loan_ds$next_pymnt_d <- as.Date(loan_ds$next_pymnt_d, format = "%d-%b-%y")
-str(loan_ds$next_pymnt_d)
-      #As lot of fields are Blank hence ignoring the field for analysis
+#As lot of fields are Blank hence ignoring the field for analysis
 remove_fields <- append(remove_fields, "next_pymnt_d")
 
 
-#7 -- Summary for last_pymnt_amnt (Last total payment amount received)
-str(loan_ds$last_pymnt_amnt)
+#Summary for last_pymnt_amnt (Last total payment amount received)
 sum(is.na(loan_ds$last_pymnt_amnt))
 sum(loan_ds$last_pymnt_amnt %in% c("", " "))
 sum(loan_ds$last_pymnt_amnt==0) #74
-
-# this field can be used to calculate profit Loss. But not sure if can be used to for default
+# this field can be used to calculate profit Loss.
+#this doesn't give historical insight of customer behaviour
+remove_fields <- append(remove_fields, "last_pymnt_amnt")
 
    
 
-#6 -- Summary for last_pymnt_d (Last month payment was received)
-str(loan_ds$last_pymnt_d)
+#Summary for last_pymnt_d (Last month payment was received)
 sum(is.na(loan_ds$last_pymnt_d))
 sum(loan_ds$last_pymnt_d %in% c("", " "))
 loan_ds$last_pymnt_d <- tolower(gsub(" ", "", loan_ds$last_pymnt_d))
-loan_ds$last_pymnt_d <- paste("01-",loan_ds$last_pymnt_d, sep = "")
+#this doesn't give historical insight of customer behaviour
+remove_fields <- append(remove_fields, "last_pymnt_d")
 
-loan_ds$last_pymnt_d <- as.Date(loan_ds$last_pymnt_d, format = "%d-%b-%y")
-str(loan_ds$last_pymnt_d)
 
-#5 -- Summary for collection_recovery_fee
-str(loan_ds$collection_recovery_fee)
+#Summary for collection_recovery_fee
 sum(is.na(loan_ds$collection_recovery_fee))
 sum(loan_ds$collection_recovery_fee %in% c("", " "))
 sum(loan_ds$collection_recovery_fee==0)#35935
-sum(loan_ds$collection_recovery_fee!=0)#3782
-          # From Business point of view this comes after the user starts defaulting/becomes delinquent, 
-          #hence not sure if this can be used for default. This can be used to study recovry cost analysis
+# From Business point of view this comes after the user starts defaulting/becomes delinquent, 
+#hence not keeping in study
+remove_fields <- append(remove_fields, "collection_recovery_fee")
 
-#4 -- Summary for recoveries (Recovery after charge off)
-str(loan_ds$recoveries)
+
+#Summary for recoveries (Recovery after charge off)
 sum(is.na(loan_ds$recoveries))
 sum(loan_ds$recoveries %in% c("", " "))
 sum(loan_ds$recoveries==0)#35499
-sum(loan_ds$recoveries!=0)#4218
-         # This also has higher no. of values with zero also this is after default. Hence can be igoned.
-         # But can be used to calculate p/L
+# This also has higher no. of values with zero also this is after default.
+#hence not keeping in study
+remove_fields <- append(remove_fields, "recoveries")
 
 
-#3 -- Summary for total_rec_late_fee (Late fees received to date)
-str(loan_ds$total_rec_late_fee)
+#Summary for total_rec_late_fee (Late fees received to date)
 sum(is.na(loan_ds$total_rec_late_fee))
 sum(loan_ds$total_rec_late_fee %in% c("", " "))
 sum(loan_ds$total_rec_late_fee==0)#37671
-sum(loan_ds$total_rec_late_fee!=0)#2046
-       # This can be analysed as people with zero late fee tend not to deffault and repay in time.
-       # But this comes after Loan is given hence Not sure if we should consider this for analysis
+# This can be analysed as people with zero late fee tend not to deffault and repay in time.
+# But this comes after Loan is given hence removing this.
+remove_fields <- append(remove_fields, "total_rec_late_fee")
 
-#2 -- Summary for total_rec_int (Interest received to date)
-str(loan_ds$total_rec_int)
+
+
+#Summary for total_rec_int (Interest received to date)
 sum(is.na(loan_ds$total_rec_int))
 sum(loan_ds$total_rec_int %in% c("", " "))
 sum(loan_ds$total_rec_int==0)#71
-sum(loan_ds$total_rec_int!=0)#39646
-       # This can be analysed as people with High interest may tend to deffault.
-       # But this comes after Loan is given hence Not sure if we should consider this for analysis
+#This is not going to tell historical behaviour of borrower
+#But this comes after Loan is given hence removing it
+remove_fields <- append(remove_fields, "total_rec_int")
 
-#1 -- Summary for total_rec_prncp (Principal received to date)
-str(loan_ds$total_rec_prncp)
+
+
+#Summary for total_rec_prncp (Principal received to date)
 sum(is.na(loan_ds$total_rec_prncp))
 sum(loan_ds$total_rec_prncp %in% c("", " "))
 sum(loan_ds$total_rec_prncp==0)#74
-sum(loan_ds$total_rec_prncp!=0)#39643
-      # This comes after Loan is given hence Not sure if we should consider this for analysis
+# This comes after Loan is given and also
+#this is not going to tell historical behaviour of borrower
+remove_fields <- append(remove_fields, "total_rec_prncp")
+
+credit_holder_since <- issue_year - loan_ds$credit_ln_year
+loan_ds <- cbind(loan_ds, credit_holder_since)
 
 
 #remove_fields
